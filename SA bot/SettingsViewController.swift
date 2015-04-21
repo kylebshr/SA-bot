112205a0e95031206b14026fb10cd75313515ba9
Kyle Bashour <kylebshr@me.com>
2015-04-21 12:58:50 -0400

    Added confirmation to logout button. Logout button is now handled by the cell instead of a button in the cell.

--- SA bot/SettingsViewController.swift
+++ SA bot/SettingsViewController.swift
@@ -29,15 +29,29 @@ class SettingsViewController: UITableViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    @IBAction func logOutWasPressed(sender: AnyObject) {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
 
-        NSUserDefaults.standardUserDefaults().setBool(false, forKey: "issuesOnly")
+        if indexPath.section == 2 && indexPath.row == 0 {
 
-        PFUser.logOutInBackgroundWithBlock({ finished in
+            tableView.deselectRowAtIndexPath(indexPath, animated: false)
 
-            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-            let welcomeController = storyBoard.instantiateViewControllerWithIdentifier("WelcomeNavController") as! UINavigationController
-            self.presentViewController(welcomeController, animated: true, completion: nil)
-        })
+            let alertController = UIAlertController(title: "Are You Sure?", message: nil, preferredStyle: .ActionSheet)
+            let logOutAction = UIAlertAction(title: "Log Out", style: .Destructive) { (action) in
+
+                NSUserDefaults.standardUserDefaults().setBool(false, forKey: "issuesOnly")
+
+                PFUser.logOutInBackgroundWithBlock({ finished in
+
+                    let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+                    let welcomeController = storyBoard.instantiateViewControllerWithIdentifier("WelcomeNavController") as! UINavigationController
+                    self.presentViewController(welcomeController, animated: true, completion: nil)
+                })
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            alertController.addAction(logOutAction)
+            alertController.addAction(cancelAction)
+
+            presentViewController(alertController, animated: true, completion: nil)
+        }
     }
 }
